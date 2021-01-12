25c11c149bd7112f52668f513a7194ad6e470795
zuhaili <19004781@myrp.edu.sg>
2021-01-12 14:31:47 +0800

    third commit

--- js/showCountryObese.js
+++ js/showCountryObese.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
-
-    
-});
+                $("#idCountry").change(function () {
+                    var countryID = $(this).val()
+                    $.ajax({
+                        type: "GET",
+                        url: "https://c273p09.azurewebsites.net/getCountryDetails.php",
+                        data: "id=" + countryID,
+                        cache: false,
+                        dataType: "JSON",
+                        success: function (response) {
+                            $(".studentid").html(response.student_id);
+                            $(".obeseTable").html(response.population);
+                            $(".obese").html(response.obese);
+                        },
+                        error: function (obj, textStatus, errorThrown) {
+                            console.log("Error " + textStatus + ": " + errorThrown);
+                        }
+                    });
+                });
+            });
