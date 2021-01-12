2a2ab56e976d6907e8ea63f3f3bc84d92c0b822b
zuhaili <19004781@myrp.edu.sg>
2021-01-12 21:36:05 +0800

    another commit

--- js/showCountryObese.js
+++ js/showCountryObese.js
@@ -8,10 +8,14 @@ $(document).ready(function () {
                         cache: false,
                         dataType: "JSON",
                         success: function (response) {
-                            var message = "<thead></th><th>Population</th><th>obese</th></tr></thead>";
+                            var message = "";
                             
                              message += "<tbody><tr><td>" + response.population + "</td><td>"
                                     + response.obese + "</td></tr></tbody>";
+                            
+//                            $(".studentid").html(response.student_id);
+//                            $(".firstname").html(response.first_name);
+//                            $(".lastname").html(response.last_name);
                             
 
                             $("#obeseTable").html(message);
--- showCountryObese.php
+++ showCountryObese.php
@@ -16,7 +16,7 @@ mysqli_close($link);
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>View Obesity and Population by country</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
         <script src="js/showCountryObese.js" type="text/javascript"></script>
 
