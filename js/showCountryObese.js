9f875050c5eb29d144ded175f4c43867ec8e7503
zuhaili <19004781@myrp.edu.sg>
2021-01-12 21:20:25 +0800

    fifth commit

--- js/showCountryObese.js
+++ js/showCountryObese.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
                     var countryID = $(this).val()
                     $.ajax({
                         type: "GET",
-                        url: "https://c273p09.azurewebsites.net/getCountryDetails.php",
+                        url: "getCountryDetails.php",
                         data: "id=" + countryID,
                         cache: false,
                         dataType: "JSON",
